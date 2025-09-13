0d6e3e09d5953c23700379924c50d6d48573417c
kurukshetra <kurukshetracodingschool.com>
2025-09-13 20:47:25 +0530

    profile section is start

---
+++ js/create.js
@@ -0,0 +1,9 @@
+let postReelStory = document.querySelector(".postReelStory .post")
+let createicon  = document.querySelector('.postReelStory i')
+let inputfile = document.querySelector('.postReelStory input')
+
+
+
+createicon.addEventListener('click',()=>{
+    inputfile.style.display = 'block'
+})
